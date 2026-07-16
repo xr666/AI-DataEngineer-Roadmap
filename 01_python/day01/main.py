7daa35965f423d349e0400a3c7d7511a3036182c
xr666 <2624343107@qq.com>
2026-07-16 18:49:18 +0800

    init: 初始化项目结构

---
+++ 01_python/day01/main.py
@@ -0,0 +1,10 @@
+"""
+Day01
+
+Author: xr666
+
+Project:
+AI Data Engineer Roadmap
+"""
+
+print("Hello AI Data Engineer")
---
+++ README.md
@@ -0,0 +1,11 @@
+# AI Data Engineer Roadmap
+
+作者：xr666
+
+目标：
+
+从数据治理工程师成长为 AI Data Engineer。
+
+开始时间：
+
+2026-07-16
